8800a276c3e9261d970839bbb139454506aec74d
YuanChin <50520156+YuanChin@users.noreply.github.com>
2022-10-10 21:47:47 +0800

    feat: 自動生成 Service 類別
    方便為控制器自動生成 Service 類別

---
+++ app/Console/Commands/GeneratesServiceClass.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class GeneratesServiceClass extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:service';
+
+    /**
+     * The console command description
+     *
+     * @var string
+     */
+    protected $description = '產生 service 類別';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return $this->laravel->basePath('/stubs/service.stub');
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Services';
+    }
+}
