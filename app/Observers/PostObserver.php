23fd9923f3cb892d29c5d852986eb8f59c1d7201
YuanChin <50520156+YuanChin@users.noreply.github.com>
2022-10-14 09:58:50 +0800

    feat: 翻譯標題作為 Slug
    引入 google-translate-php 進行翻譯

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Jobs\TranslateSlug;
 use App\Models\Post;
 
 class PostObserver
@@ -16,5 +17,21 @@ class PostObserver
     {
         // 防止XSS注入
         $post->body = clean($post->body);
+    }
+
+    /**
+     * 文章更新後觸發的事件
+     *
+     * @param Post $post
+     * @return void
+     */
+    public function updated(Post $post)
+    {
+        // 佇列處理翻譯SLUG
+        if (!app()->runningInConsole()) {
+            if (!$post->slug) {
+                dispatch(new TranslateSlug($post));
+            }
+        }
     }
 }
